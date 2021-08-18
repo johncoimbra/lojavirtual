13082ec8eaa11ecfd9e81e04953895a6b2b72dca
John Coimbra <johnallen.inf@gmail.com>
2021-08-18 20:17:43 -0300

    Selecting Photos from the Gallery

--- app/src/main/java/com/johncoimbra/lojavirtiual/fragments/CadastroProdutosActivity.kt
+++ app/src/main/java/com/johncoimbra/lojavirtiual/fragments/CadastroProdutosActivity.kt
@@ -1,12 +1,50 @@
 package com.johncoimbra.lojavirtiual.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import com.johncoimbra.lojavirtiual.R
+import com.johncoimbra.lojavirtiual.databinding.ActivityCadastroProdutosBinding
 
 class CadastroProdutosActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCadastroProdutosBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro_produtos)
+        binding = ActivityCadastroProdutosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val getImage = registerForActivityResult(
+            ActivityResultContracts.GetContent(),
+            ActivityResultCallback {
+                binding.fotoProduto.setImageURI(it)
+                binding.btSelecionarFoto.alpha = 0f
+            }
+        )
+
+        binding.btSelecionarFoto.setOnClickListener {
+            getImage.launch("image/*")
+
+//            val intent = Intent(Intent.ACTION_PICK)
+//            intent.type = "image/*"
+//            startActivityForResult(intent, 0)
+        }
     }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == 0) {
+//            selectUri = data?.data
+//            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectUri)
+//            binding.fotoProduto.setImageBitmap(bitmap)
+//            binding.btSelecionarFoto.alpha = 0f
+//        }
+//    }
 }
--- app/src/main/java/com/johncoimbra/lojavirtiual/ui/MainScreenActivity.kt
+++ app/src/main/java/com/johncoimbra/lojavirtiual/ui/MainScreenActivity.kt
@@ -1,23 +1,17 @@
-package com.johncoimbra.lojavirtiual
+package com.johncoimbra.lojavirtiual.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import com.johncoimbra.lojavirtiual.R
 import com.johncoimbra.lojavirtiual.databinding.ActivityMainScreenBinding
 import com.johncoimbra.lojavirtiual.form.LoginActivity
 import com.johncoimbra.lojavirtiual.fragments.CadastroProdutosActivity
@@ -38,7 +32,10 @@ class MainScreenActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, binding.appBarMainScreen.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+            this, drawerLayout, binding.appBarMainScreen.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
@@ -46,11 +43,11 @@ class MainScreenActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
-        if(id==R.id.nav_produtos){
+        if(id== R.id.nav_produtos){
             loadFragment(ProdutosFragment())
-        }else if(id==R.id.nav_cadastrar_produtos){
+        }else if(id== R.id.nav_cadastrar_produtos){
             startActivity(Intent(this, CadastroProdutosActivity::class.java))
-        }else if(id==R.id.nav_contato){
+        }else if(id== R.id.nav_contato){
 
         }
 
